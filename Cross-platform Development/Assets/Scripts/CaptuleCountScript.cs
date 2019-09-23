39ae5d39d5db09e14a5b198efcbcd8c18c5c7b00
Josh Francis <joshua.francis@live.com.au>
2019-09-23 15:41:29 +1000

    Added count for amount of captules

---
+++ Cross-platform Development/Assets/Scripts/CaptuleCountScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaptuleCountScript : MonoBehaviour
+{
+	TextMesh text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		text = GetComponent<TextMesh>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		text.text = GameObject.FindGameObjectsWithTag("Shootable").Length.ToString();
+
+	}
+}
